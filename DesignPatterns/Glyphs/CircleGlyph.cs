2241b1f970506a012d0cbc2582753c338533c8a6
Brendan <brendanrahn01@gmail.com>
2025-05-17 15:46:22 -0500

    add circle glyph

---
+++ DesignPatterns/Glyphs/CircleGlyph.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Iterator.Iterators;
+
+namespace Iterator.Glyphs
+{
+    public class CircleGlyph : IGlyph
+    {
+        private string _circleDisplay;
+        public CircleGlyph()
+        {
+            var builder = new StringBuilder();
+            builder.Append("    __\n");
+            builder.Append("  |    |\n");
+            builder.Append("|        |\n");
+            builder.Append("|        |\n");
+            builder.Append("  | __ |\n");
+            _circleDisplay = builder.ToString();
+        }
+
+        public IIterator CreateIterator()
+        {
+            return new NullIterator();
+        }
+
+        public string Display()
+        {
+            return _circleDisplay;
+        }
+    }
+}
