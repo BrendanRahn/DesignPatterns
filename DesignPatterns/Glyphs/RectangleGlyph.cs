f926f772979ec4c074874a14b3c701813f3a961a
Brendan <brendanrahn01@gmail.com>
2025-05-17 15:23:48 -0500

    add rectangle glyph

---
+++ DesignPatterns/Glyphs/RectangleGlyph.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Iterator.Iterators;
+
+namespace Iterator.Glyphs
+{
+    public class RectangleGlyph : IGlyph
+    {
+        private string _rectangleDisplay;
+        public RectangleGlyph()
+        {
+            var builder = new StringBuilder();
+            builder.Append("----\n");
+            builder.Append("|  |\n");
+            builder.Append("----\n");
+            _rectangleDisplay = builder.ToString();
+        }
+
+        public IIterator CreateIterator()
+        {
+            return new NullIterator();
+        }
+
+        public string Display()
+        {
+            return _rectangleDisplay;
+        }
+    }
+}
