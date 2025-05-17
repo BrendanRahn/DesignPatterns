1fc32b8040a2b112432f8ed5c58881342378a93a
Brendan <brendanrahn01@gmail.com>
2025-05-17 15:46:40 -0500

    pull up console on app launch that displays some glyphs

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Iterator.Glyphs;
 
 namespace Iterator
 {
@@ -10,7 +11,36 @@ namespace Iterator
     {
         static void Main(string[] args)
         {
-            //do nothing?
+            var word1 = new ArrayGlyph(new List<IGlyph>()
+            {
+                new CharacterGlyph('S'),
+                new CharacterGlyph('t'),
+                new CharacterGlyph('a'),
+                new CharacterGlyph('r'),
+                new CharacterGlyph('t'),
+                new CharacterGlyph('\n'),
+            });
+            var rectangle = new RectangleGlyph();
+            var circle = new CircleGlyph();
+            var word2 = new ArrayGlyph(new List<IGlyph>()
+            {
+                new CharacterGlyph('E'),
+                new CharacterGlyph('n'),
+                new CharacterGlyph('d'),
+                new CharacterGlyph('\n'),
+            });
+
+
+            var displayManyGlyphs = new ArrayGlyph(new List<IGlyph>()
+            {
+                word1,
+                rectangle,
+                circle,
+                word2,
+            });
+            Console.Write(displayManyGlyphs.Display()); 
+
+
         }
     }
 }
