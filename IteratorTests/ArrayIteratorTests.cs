ce7e8fbbf0a1099393337b1fc100204af0f2f5aa
Brendan <brendanrahn01@gmail.com>
2025-05-17 21:09:53 -0500

    update arrayIterator tests with insert behavior update

---
+++ IteratorTests/ArrayIteratorTests.cs
@@ -0,0 +1,90 @@
+﻿
+
+using Iterator;
+using Iterator.Glyphs;
+
+namespace IteratorTests
+{
+    [TestClass]
+    public sealed class ArrayIteratorTests
+    {
+        [TestMethod]
+        public void ArrayIterator_IteratesAllChildren()
+        {
+            var glyphs = new List<IGlyph>
+            {
+                new CharacterGlyph('H'),
+                new CharacterGlyph('e'),
+                new CharacterGlyph('l'),
+                new CharacterGlyph('l'),
+                new CharacterGlyph('o')
+            };
+            var word = new ArrayGlyph(glyphs);
+
+            var i = word.CreateIterator();
+
+            i.First();
+            Assert.AreEqual("H", i.GetCurrent().Display());
+
+            i.Next();
+            Assert.AreEqual("e", i.GetCurrent().Display());
+
+            i.Next();
+            Assert.AreEqual("l", i.GetCurrent().Display());
+
+            i.Next();
+            Assert.AreEqual("l", i.GetCurrent().Display());
+
+            i.Next();
+            Assert.AreEqual("o", i.GetCurrent().Display());
+
+        }
+
+        [TestMethod]
+        public void ArrayIterator_InsertsGlyph()
+        {
+            var glyphs = new List<IGlyph>
+            {
+                new CharacterGlyph('A'),
+                new CharacterGlyph('B'),
+            };
+
+            var word = new ArrayGlyph(glyphs);
+            var i = word.CreateIterator();
+
+            i.First();
+            Assert.AreEqual("A", i.GetCurrent().Display());
+            
+            i.Insert(new CharacterGlyph('C'));
+            i.Next();
+            Assert.AreEqual("C", i.GetCurrent().Display());
+
+            i.Next();
+            Assert.AreEqual("B", i.GetCurrent().Display());
+        }
+
+        [TestMethod]
+        public void ArrayIterator_SignalsIsDone()
+        {
+            var glyphs = new List<IGlyph>
+            {
+                new CharacterGlyph('H'),
+                new CharacterGlyph('e'),
+                new CharacterGlyph('l'),
+                new CharacterGlyph('l'),
+                new CharacterGlyph('o')
+            };
+            var word = new ArrayGlyph(glyphs);
+
+            var i = word.CreateIterator();
+
+            while (!i.IsDone()) {
+                i.GetCurrent().Display();
+                i.Next();
+
+            }
+
+            Assert.IsTrue(i.IsDone());
+        }
+    }
+}
